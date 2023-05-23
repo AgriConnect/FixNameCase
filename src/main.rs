f59d1c1ed3a2ec45ea1411b3a6c4891bf2eab369
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-05-23 15:23:15 +0700

    Regard gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use std::process::Command;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
 use std::{ops::RangeInclusive, str};
 
+use case_converter::camel_to_snake;
 use color_eyre::eyre::Context;
 use color_eyre::Result;
 use eyre::eyre;
+use ignore::{DirEntry, Walk};
 use serde::{Deserialize, Serialize};
-use case_converter::camel_to_snake;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Tag {
     name: String,
+    path: PathBuf,
     kind: String,
 }
 
@@ -19,20 +23,41 @@ const UPPER_LETTERS: RangeInclusive<u8> = b'A'..=b'Z';
 fn main(folder: std::path::PathBuf) -> Result<()> {
     color_eyre::install()?;
     folder.try_exists().wrap_err("Input folder not exist")?;
-    let folder_path = folder.to_str().ok_or(eyre!("Invalid name"))?;
+    let entries: Vec<DirEntry> = Walk::new(&folder).filter_map(|entry| entry.ok()).collect();
+    let _folder_path = folder.to_str().ok_or(eyre!("Invalid name"))?;
     let mut command = Command::new("ctags");
+    command.stdin(Stdio::piped());
+    command.stdout(Stdio::piped());
     command.args([
         "--output-format=json",
-        "-R",
-        "--exclude=node_modules",
-        "--exclude=.pio",
-        "--exclude=test",
-        "--language=c++",
-        folder_path,
+        "-L",
+        "-",
     ]);
-    let cout = command.output()?;
-    let soutput = std::str::from_utf8(&cout.stdout)?;
-    let lines = soutput.trim().split("\n");
+    let process = command.spawn()?;
+    let file_paths: Vec<String> = entries
+        .into_iter()
+        .filter_map(|d| {
+            let path = d.into_path().into_os_string().into_string().ok();
+            path.map(|p| {
+                if p.ends_with(".c") || p.ends_with(".cpp") || p.ends_with(".h") {
+                    Some(p)
+                } else {
+                    None
+                }
+            })?
+        })
+        .collect();
+    let m = file_paths.join("\n");
+    process
+        .stdin
+        .ok_or(eyre!("Failed to grab stdin"))?
+        .write_all(m.as_bytes())?;
+    let mut out = String::new();
+    process
+        .stdout
+        .ok_or(eyre!("Can not read ctags output"))
+        .map(|mut s| s.read_to_string(&mut out))??;
+    let lines = out.trim().split("\n");
     let tags: Vec<Tag> = lines
         .filter_map(|l| {
             let tag = serde_json::from_str::<Tag>(l.trim()).ok()?;
@@ -41,7 +66,7 @@ fn main(folder: std::path::PathBuf) -> Result<()> {
         .collect();
     let variables: Vec<Tag> = tags
         .into_iter()
-        .filter(|t| (t.kind == "variable" || t.kind == "function") && t.name != "File")
+        .filter(|t| (t.kind == "variable" || t.kind == "function"))
         .collect();
     let var_names: Vec<String> = variables.into_iter().map(|t| t.name).collect();
     let non_snake_names: Vec<String> = var_names
